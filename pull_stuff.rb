4889823e1a55d1563c7eb2c2a3bb9f2d0c5d55a8
Roman Heinrich <roman.heinrich@gmail.com>
2013-06-19 23:44:04 +0200

    update own readme file

--- pull_stuff.rb
+++ pull_stuff.rb
@@ -241,3 +241,10 @@ end
 pe = ProjectsExecuter.new
 #pe.update_repos
 pe.update_projects_lists
+
+#### update top readme
+my_projects  = FOLDERS.map{|x| "http://github.com/mindreframer/#{x}"}
+readmewriter = ReadmeWriter.new('.')
+readmewriter.add_projects_list(my_projects)
+readmewriter.git_commit
+readmewriter.git_push_if_changed
